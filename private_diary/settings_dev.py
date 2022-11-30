a730d4bde5abbb4edb99dfafcf623fb9c12538b7
j4pngthr <weibeicun@gmail.com>
2022-11-30 11:36:58 +0900

    問い合わせページのフォーム機能を実装

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,52 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-n4-w3jaat&+93ue_ir_*4*-2x2(zhi&a$)ztr4%a12p!i6q++q'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version': 1, # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
+
+# メールの配信先をコンソールに
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
